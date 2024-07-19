2bd298539a69daf9985412c1b4841a0c66c6def5
Piyush Gupta <piyushpappugupta@gmail.com>
2024-07-19 19:44:49 +0530

    second commit

--- UserManagement/settings.py
+++ UserManagement/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$-npv1sbwj^d%)l0lhl5qfq65y4xhv0)a2%dw4@x+686y-s)-n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'UserManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
